0bbb80bbee8f507affe7a44b957a9a3048655162
Sam <sambyoung@gmail.com>
2020-03-14 13:51:24 -0400

    first commit- rough rough version

---
+++ database/scripts/counter.js
@@ -0,0 +1,42 @@
+let lastGroupCountRef = firebase.database().ref('lastGroup');
+let maxGroupCountRef = firebase.database().ref('maxGroup');
+lastGroupCountRef.on('child_changed', function(data) {
+  window.lastCount = data.val();
+});
+
+async function getValues() {
+  
+  window.lastCount = await lastGroupCountRef.once('value', function(snapshot) {
+    return snapshot;
+  })
+    .then(function(snapshot) {
+      return window.lastCount = snapshot.val().count;
+    });
+		
+  window.maxCount = await maxGroupCountRef.once('value', function(snapshot) {
+    return snapshot;
+  })
+    .then(function(snapshot) {
+      return window.maxCount = snapshot.val().count;
+    });
+}
+
+function setGroupCount() {
+  if (window.lastCount === window.maxCount) {
+    window.nextGroup = 1;
+    alert(`group is ${window.nextGroup}`);
+  } else {
+    window.nextGroup = window.lastCount + 1;
+    alert(`group is ${window.nextGroup}`);
+  }
+  updateLastCountDb();
+}
+
+function updateLastCountDb() {
+  lastGroupCountRef.update({count: window.nextGroup});
+}
+
+async function testIt() {
+  await setGroupCount();
+  updateLastCountDb();
+}
